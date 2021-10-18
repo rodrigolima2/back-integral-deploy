fe19af1da5ddc12b0543284e1b2db4d148ddc7ee
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-10-18 19:04:19 -0300

    Configurações deploy

--- src/conexao.js
+++ src/conexao.js
@@ -1,10 +1,14 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'postgres',
-        password: 'asd',
-        database: 'market_cubos_querybuilder'
+        host: 'ec2-52-70-107-254.compute-1.amazonaws.com',
+        user: 'dknwlkqirnwaue',
+        password: 'c5e2844e7ac0421c88611e3a99f1759384aa852fa95ac8807939632a562f68f7',
+        database: 'd923p3a7pphbod',
+        port: 5432,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
