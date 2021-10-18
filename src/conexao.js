c6d4ff43e8a636961cfd69f94f591b78a79c9631
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-10-18 19:46:22 -0300

    Deploy

--- src/conexao.js
+++ src/conexao.js
@@ -1,10 +1,10 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'ec2-52-70-107-254.compute-1.amazonaws.com',
         user: 'dknwlkqirnwaue',
-        password: 'c5e2844e7ac0421c88611e3a99f1759384aa852fa95ac8807939632a562f68f7',
+        host: 'ec2-52-70-107-254.compute-1.amazonaws.com',
         database: 'd923p3a7pphbod',
+        password: 'c5e2844e7ac0421c88611e3a99f1759384aa852fa95ac8807939632a562f68f7',
         port: 5432,
         ssl: {
             rejectUnauthorized: false
